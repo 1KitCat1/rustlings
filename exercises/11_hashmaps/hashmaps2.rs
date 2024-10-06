549c29578561acf48634fb149004bd1f081eb16a
Denis Riabtsev <denischool119@gmail.com>
2024-10-06 20:04:25 +0300

    Finished hashmaps

--- exercises/11_hashmaps/hashmaps2.rs
+++ exercises/11_hashmaps/hashmaps2.rs
@@ -32,11 +32,25 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Insert new fruits if they are not already present in the
         // basket. Note that you are not allowed to put any type of fruit that's
         // already present!
+        basket.entry(fruit).or_insert(1);
     }
 }
 
 fn main() {
-    // You can optionally experiment here.
+    // let mut scores = HashMap::new();
+    // scores.insert(String::from("Blue"), 10);
+
+    // scores.entry(String::from("Yellow")).or_insert(50);
+    // scores.entry(String::from("Blue")).or_insert(50);
+
+    // println!("{scores:?}");
+
+    let mut scores = HashMap::new();
+    scores.insert(String::from("a"), 10);
+    scores.entry(String::from("b")).or_insert(11);
+    scores.insert(String::from("a"), 12);
+    println!("{scores:?}");
+    println!("a: {}", scores.get(&String::from("a")).unwrap_or(&0));
 }
 
 #[cfg(test)]
